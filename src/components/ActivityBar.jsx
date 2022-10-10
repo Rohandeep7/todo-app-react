ad9f5460ff6f4ca31f6cf97c15f1a8a3ec0af038
Rohandeep Munipally <rohandeep.m@comakeit.com>
2022-10-10 11:16:02 +0530

    Activity Bar

--- src/components/ActivityBar.jsx
+++ src/components/ActivityBar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 function ActivityBar() {
+  const [active, setActive] = useState("");
   return (
     <>
-      
+      <div className="activity">
+        <div className="activity-cont">
+          <NavLink
+            to="/"
+            activeStyle={{
+              height: "4px",
+            }}
+            className="all-todos cont active"
+          >
+            All
+          </NavLink>
+          <NavLink
+            to="/"
+            activeStyle={{
+              height: "4px",
+            }}
+            className="active-todos cont"
+          >
+            Active
+          </NavLink>
+          <NavLink
+            to="/"
+            activeStyle={{
+              height: "4px",
+            }}
+            className="completed-todos cont"
+          >
+            Completed
+          </NavLink>
+        </div>
+      </div>
     </>
   );
 }
 
-export default ActivityBar
+export default ActivityBar;
